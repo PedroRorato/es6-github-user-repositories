8152ee826a6040ef88dd3bf64c39af2322f7c21f
Pedro <pedroberorato@gmail.com>
2020-08-24 20:02:01 -0300

    HTML Completed

--- src/main.js
+++ src/main.js
@@ -64,22 +64,19 @@ class App {
             let imgEl = document.createElement('img');
             imgEl.setAttribute('src', repo.avatar_url);
 
-            let titleEl = document.createElement('strong');
-            titleEl.appendChild(document.createTextNode(repo.name));
-
             let descriptionEl = document.createElement('p');
             descriptionEl.appendChild(document.createTextNode(repo.description));
 
             let linkEl = document.createElement('a');
             linkEl.setAttribute('target', '_blank');
             linkEl.setAttribute('href', repo.html_url);
-            linkEl.appendChild(document.createTextNode('Acessar'));
+            linkEl.appendChild(document.createTextNode(repo.name));
 
             let listItemEl = document.createElement('li');
             listItemEl.appendChild(imgEl);
-            listItemEl.appendChild(titleEl);
-            listItemEl.appendChild(descriptionEl);
             listItemEl.appendChild(linkEl);
+            listItemEl.appendChild(descriptionEl);
+            
 
             this.listEl.appendChild(listItemEl);
         });
